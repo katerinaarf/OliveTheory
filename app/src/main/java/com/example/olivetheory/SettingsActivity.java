dd9d2408ef6e049384d0d9a314f6cb4c68060f5d
katerinaarf <katerina.arf@hotmail.com>
2024-05-20 02:38:01 +0300

    omw to make the cutest settings

--- app/src/main/java/com/example/olivetheory/SettingsActivity.java
+++ app/src/main/java/com/example/olivetheory/SettingsActivity.java
@@ -19,7 +19,7 @@ public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu);
+        setContentView(R.layout.activity_settings);
 
 
 
