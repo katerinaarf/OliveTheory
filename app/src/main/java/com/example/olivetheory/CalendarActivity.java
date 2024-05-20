07ef962a50e3ed7e93d33888eb0b6ba6ad000ab4
katerinaarf <katerina.arf@hotmail.com>
2024-05-20 03:06:33 +0300

    omw to make the cutest calendar

--- app/src/main/java/com/example/olivetheory/CalendarActivity.java
+++ app/src/main/java/com/example/olivetheory/CalendarActivity.java
@@ -1,4 +1,89 @@
 package com.example.olivetheory;
 
-public class CalendarActivity {
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.w3c.dom.Text;
+
+public class CalendarActivty extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calendar);
+
+//        //Text
+//        TextView weatherText = findViewById(R.id.weatherText);
+//        TextView problemsText = findViewById(R.id.problemsText);
+
+        // Buttons
+        Button settingButton = findViewById(R.id.calendar_second);
+        Button calendarButton = findViewById(R.id.calendar);
+        Button weatherButton = findViewById(R.id.weather);
+        ImageButton problemsButton = findViewById(R.id.problems);
+        Button messageButton = findViewById(R.id.message);
+        Button forumButton = findViewById(R.id.forum);
+
+
+        //Change Activity
+
+        calendarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent calendarsecActivity = new Intent(CalendarActivty.this, CalendarActivty.class);
+                startActivity(calendarsecActivity);
+            }
+        });
+
+        calendarButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent calendarActivity = new Intent(CalendarActivty.this, CalendarActivty.class);
+                startActivity(calendarActivity);
+            }
+        });
+
+        weatherButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent weatherActivity = new Intent(CalendarActivty.this, WeatherActivity.class);
+                startActivity(weatherActivity);
+            }
+        });
+
+        problemsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent problemsActivity = new Intent(CalendarActivty.this, MapsActivity.class);
+                startActivity(problemsActivity);
+            }
+        });
+
+        messageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent messageActivity = new Intent(CalendarActivty.this, MenuActivity.class);
+                startActivity(messageActivity);
+            }
+        });
+
+        forumButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent forumActivity = new Intent(CalendarActivty.this, ForumActivity.class);
+                startActivity(forumActivity);
+            }
+        });
+    }
 }
+
+
