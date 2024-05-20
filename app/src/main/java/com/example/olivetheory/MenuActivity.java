7df582685395aa52f0a3903f80b33b9e0e758d1f
VasoLiak <vasilikiliak@gmail.com>
2024-05-20 04:33:14 +0300

    FIREBASE sign up

--- app/src/main/java/com/example/olivetheory/MenuActivity.java
+++ app/src/main/java/com/example/olivetheory/MenuActivity.java
@@ -22,63 +22,63 @@ public class MenuActivity extends AppCompatActivity {
 //        TextView problemsText = findViewById(R.id.problemsText);
 
         // Buttons
-        Button settingButton = findViewById(R.id.settings);
-        Button calendarButton = findViewById(R.id.calendar);
-        Button weatherButton = findViewById(R.id.weather);
-        Button problemsButton = findViewById(R.id.problems);
-        Button messageButton = findViewById(R.id.message);
-        Button forumButton = findViewById(R.id.forum);
-
-
-        //Change Activity
-
-        settingButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent settingsActivity = new Intent(MenuActivity.this, SettingsActivity.class);
-                startActivity(settingsActivity);
-            }
-        });
-
-        calendarButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent calendarActivity = new Intent(MenuActivity.this, CalendarActivity.class);
-                startActivity(calendarActivity);
-            }
-        });
-
-        weatherButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent weatherActivity = new Intent(MenuActivity.this, WeatherActivity.class);
-                startActivity(weatherActivity);
-            }
-        });
-
-        problemsButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent problemsActivity = new Intent(MenuActivity.this, ProblemsActivity.class);
-                startActivity(problemsActivity);
-            }
-        });
-
-        messageButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent messageActivity = new Intent(MenuActivity.this, MenuActivity.class);
-                startActivity(messageActivity);
-            }
-        });
-
-        forumButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MenuActivity.this, ForumActivity.class);
-                startActivity(intent);
-            }
-        });
+//        Button settingButton = findViewById(R.id.settings);
+//        Button calendarButton = findViewById(R.id.calendar);
+//        Button weatherButton = findViewById(R.id.weather);
+//        Button problemsButton = findViewById(R.id.problems);
+//        Button messageButton = findViewById(R.id.message);
+//        Button forumButton = findViewById(R.id.forum);
+//
+//
+//        //Change Activity
+//
+//        settingButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent settingsActivity = new Intent(MenuActivity.this, SettingsActivity.class);
+//                startActivity(settingsActivity);
+//            }
+//        });
+//
+//        calendarButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent calendarActivity = new Intent(MenuActivity.this, CalendarActivity.class);
+//                startActivity(calendarActivity);
+//            }
+//        });
+//
+//        weatherButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent weatherActivity = new Intent(MenuActivity.this, WeatherActivity.class);
+//                startActivity(weatherActivity);
+//            }
+//        });
+//
+//        problemsButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent problemsActivity = new Intent(MenuActivity.this, ProblemsActivity.class);
+//                startActivity(problemsActivity);
+//            }
+//        });
+//
+//        messageButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent messageActivity = new Intent(MenuActivity.this, MenuActivity.class);
+//                startActivity(messageActivity);
+//            }
+//        });
+//
+//        forumButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(MenuActivity.this, ForumActivity.class);
+//                startActivity(intent);
+//            }
+//        });
     }
 }
 
